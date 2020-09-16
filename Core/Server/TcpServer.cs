6d41a4b4d2e5a58a5a433e3cf4424638408c5265
钲皓 黄 <a1048654629@hotmail.com>
2020-09-16 19:08:36 +0800

    Incomplete update

--- Core/Server/TcpServer.cs
+++ Core/Server/TcpServer.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using NetworkHandler;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Core.Server
 {
-    class TcpServer
+    public static class TcpServer
     {
+
+        public static Server_TCP tcpServer = new Server_TCP(Setting.DATA_PORT, Setting.BUFFER_SIZE, TcpMessageReceived);
+
+        public static void Init(int port, int bufferSize)
+        {
+
+        }
+
     }
 }
